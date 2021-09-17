2a79eef2f24ad0c82805d62038a5b43fd0c5c382
AlfredoDeCarglass <alfredo.cft@gmail.com>
2021-09-17 20:01:13 +0200

    Api Request Validation

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { signup, signin, requireSignin } = require('../../controller/admin/auth')
+const { signup, signin } = require('../../controller/admin/auth');
+const { validateSignUpRequest, validateSignInRequest, isRequestValidated } = require('../../validators/auth');
 const router = express.Router();
+
  // Utilice la clase express.Router para crear manejadores de rutas montables y modulares.
  // Una instancia Router es un sistema de middleware y direccionamiento completo; por este motivo,
- //  a menudo se conoce como una “miniaplicación”.
+ // a menudo se conoce como una “miniaplicación”.
 
-router.post('/admin/signup', signup);
-router.post('/admin/signin', signin);
+router.post('/admin/signup', validateSignUpRequest, isRequestValidated, signup);
+router.post('/admin/signin', validateSignInRequest, isRequestValidated, signin);
 
 
 module.exports = router;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { signup, signin, requireSignin } = require('../controller/auth')
+const { signup, signin} = require('../controller/auth');
+const { validateSignUpRequest, validateSignInRequest, isRequestValidated } = require('../validators/auth');
 const router = express.Router();
  // Utilice la clase express.Router para crear manejadores de rutas montables y modulares.
  // Una instancia Router es un sistema de middleware y direccionamiento completo; por este motivo,
  //  a menudo se conoce como una “miniaplicación”.
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post('/signup', validateSignUpRequest, isRequestValidated, signup);
+router.post('/signin', validateSignInRequest, isRequestValidated, signin);
 
 /*
 router.post('/profile', requireSignin, (req, res) => {
